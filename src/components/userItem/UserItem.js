52e446cc9788f036f0e2ed085a472545a1692893
Ramil Galikeev <galikeevramil95@gmail.com>
2022-02-02 13:11:53 +0500

    added user card component

---
+++ src/components/userItem/UserItem.js
@@ -0,0 +1,17 @@
+import './userItem.scss';
+import '../../style/fontSize.scss';
+
+const UserItem = (props) => {
+    return (
+        <div className='user'>
+            <div className='user__descr fz_11'>ФИО: <span>{props.name}</span></div>
+            <div className='user__descr fz_11'>город: <span>{props.address.city}</span></div>
+            <div className='user__wrapper'>
+                <div className='user__descr fz_11'>компания: <span>{props.company.name}</span></div>
+                <button className='user__btn fz_12'>Подробнее</button>
+            </div>
+        </div>
+    )
+}
+
+export default UserItem;
