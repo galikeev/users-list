90a61a4725727f99771644b04b74df40ae0a61b7
Ramil Galikeev <galikeevramil95@gmail.com>
2022-02-02 13:10:19 +0500

    get users data

---
+++ src/services/UsersService.js
@@ -0,0 +1,22 @@
+import { useHttp } from "../hooks/http.hook";
+
+const useUsersService = () => {
+    const {request, clearError, process, setProcess} = useHttp();
+
+    const _baseApi = 'https://jsonplaceholder.typicode.com/users';
+
+    const getUsers = async () => {
+        const res = await request(`${_baseApi}`);
+        console.log(res);
+        return res;
+    }
+
+    return {
+        clearError,
+        process,
+        setProcess,
+        getUsers
+    }
+}
+
+export default useUsersService;
